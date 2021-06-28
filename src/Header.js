d81f1100de5b6bea256c44209082215242c4b346
Ssuryavansham <sidhantsuryavansham60@gmail.com>
2021-06-29 02:34:50 +0530

    all files updated

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import "./Header.css"
+import Images from "../src/Images/circle-infor.svg"
 const Header = () => {
     return (
       <>
      <div className="container-fluid" id="cont4">
-     <h1> Sidhant here </h1>
+     <img src = {Images}></img><spam>READ ABOUT OUR COVID-19 PRECAUTIONS</spam>
      </div>
       </>
     );
