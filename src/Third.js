63d9248925576fe3fa8b1dca05125a766cf5fce2
Ssuryavansham <sidhantsuryavansham60@gmail.com>
2021-07-02 01:58:08 +0530

    7/1/2021

--- src/Third.js
+++ src/Third.js
@@ -1,22 +1,11 @@
 import React from 'react'
 import "./App.css"
-import vid from "../src/Images/vid.mp4"
+
 import poster from "../src/Images/main.jpeg"
 
 const Third = () => {
   return (
     <>
-
-<div className="poster">
-    < ul>
-    <li>a</li>
-    <li>cb</li>
-    <li>c</li>
-    <li>d</li>
-    </ul>
-</div>
-
-
 
 </>
 
