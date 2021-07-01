30b52f599531e7edbb49106a8c2affc38ff51fb2
Ssuryavansham <sidhantsuryavansham60@gmail.com>
2021-07-02 02:15:26 +0530

    7/1/2021

--- src/Third.js
+++ src/Third.js
@@ -7,6 +7,17 @@ const Third = () => {
   return (
     <>
 
+<div className="poster">
+   <h1>About <spam>My Self</spam> </h1>
+   <p>My name is Sidhant Suryavansham and i'am a PG student , curently i'm purshing MCA from ADYPU Pune Maharstra .
+    I like to play Games like Velorant , BGMI etc. I have a good knowledge in Python , HTML , CSS , React, Bootstrap.
+    I'm a good leraner , on free time i like to leran new things like new Tecnology , programing languages etc .
+    That's all about my self , By
+   </p>
+</div>
+
+
+
 </>
 
 
